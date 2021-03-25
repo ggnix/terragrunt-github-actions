cd877c9cf0d782485fc195e843b3994867683db6
Gleb Golubiatnikov <gleb.golubiatnikov@gmail.com>
2021-03-25 15:38:10 +0200

    change validate step

--- src/main.sh
+++ src/main.sh
@@ -153,6 +153,7 @@ function main {
   source ${scriptDir}/terragrunt_fmt.sh
   source ${scriptDir}/terragrunt_init.sh
   source ${scriptDir}/terragrunt_validate.sh
+  source ${scriptDir}/terragrunt_validate_all.sh
   source ${scriptDir}/terragrunt_plan.sh
   source ${scriptDir}/terragrunt_apply.sh
   source ${scriptDir}/terragrunt_output.sh
@@ -175,10 +176,6 @@ function main {
       terragruntInit ${*}
       ;;
     validate)
-      installTerragrunt
-      terragruntValidate ${*}
-      ;;
-    run-all validate)
       installTerragrunt
       terragruntValidateAll ${*}
       ;;
